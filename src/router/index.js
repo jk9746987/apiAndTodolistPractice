14ac55164f55da7ebbce9501d0c4916d0803877c
jk9746987 <jk9746987@gmail.com>
2022-07-25 16:43:27 +0800

    feat：新增todo路由
    調整：
    1.新增todo.vue做為登入成功後的路由
    2.在vue.config.js裡新增publicPath，讓專案可在github上使用

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import Todo from '@/views/Todo.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/todo',
+    name: 'Todo',
+    component: Todo
+  }
   // {
   //   path: '/about',
   //   name: 'About',
---
+++ src/views/Todo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="d-flex justify-center align-center">
+    <v-row class="col-6 d-flex justify-center align-center flex-wrap">
+      <v-col class="col-12">
+        <v-card>
+          <v-card-title>8e9wfe98q19e8w</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
