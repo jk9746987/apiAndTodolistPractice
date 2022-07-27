16273011a718304a3f5856d07ce860d1e6d61ceb
jk9746987 <jk9746987@gmail.com>
2022-07-27 19:54:19 +0800

    feat：todo頁新增相關功能
    調整：
    1.新增addTodo函式，透過點擊「+」號icon或keyup.enter新增待辦事項
    2.新增editTodo函式，透過點擊「編輯」icon可編輯待辦事項
    3.編輯完後透過confirmEdit函式確認編輯，可點選打勾或使用enter鍵，esc鍵則為取消
    4.新增deleteTodo函式，點擊後刪除該欄位待辦項目
    5.新增clearComplete函式，點擊清除已完成項目後可清除
    6.新增兩個computed，一個為紀錄左下角待完成項目的todoStatus
    另一個則為點擊上方全部、待完成、已完成後重新抓取待辦項目

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -15,77 +15,106 @@
         <div>登出</div>
       </div>
     </div>
-    <div class="outside d-flex justify-center align-center mx-auto">
-      <div class="inside mx-auto">
-        <div class="input_container mb-6">
-          <v-text-field
-            solo
-            v-model="test"
-            append-icon="mdi-plus-box"
-            x-large
-            placeholder="新增待辦事項"
-            @click:append="testfn"
-            class="rounded-lg"
-            height="50"
-          ></v-text-field>
-        </div>
-        <div class="todo_content rounded-lg pb-5">
+    <div class="outside mx-auto pb-4">
+      <div class="input_container mb-6">
+        <v-text-field
+          solo
+          v-model.trim="newTodo"
+          append-icon="mdi-plus-box"
+          x-large
+          @click="todoField = null"
+          placeholder="新增待辦事項"
+          @click:append="addTodo"
+          class="rounded-lg"
+          height="50"
+          @keyup.enter="addTodo"
+        ></v-text-field>
+      </div>
+      <div class="todo_content rounded-lg pb-5 mb-4">
+        <div class="status_container d-flex justify-space-around align-center">
           <div
-            class="status_container d-flex justify-space-around align-center"
+            v-for="(item, index) in status"
+            :key="index"
+            @click="choice_status(item, index)"
+            class="col-4 text-center"
+            :class="count === index ? 'active' : ''"
           >
-            <div
-              v-for="(item, index) in status"
-              :key="index"
-              @click="choice_status(index)"
-              class="col-4 text-center"
-              :class="count === index ? 'active' : ''"
-            >
-              {{ item }}
+            {{ item }}
+          </div>
+        </div>
+        <div class="todo_detail" v-for="item in filterTodoList" :key="item.id">
+          <div
+            class="
+              item_container
+              d-flex
+              justify-space-between
+              align-center
+              px-7
+              mx-2
+              mb-3
+              mt-4
+            "
+            v-if="todoField !== item"
+          >
+            <div class="txt_container d-flex">
+              <v-icon
+                class="mr-3"
+                v-if="item.done === false"
+                @click="item.done = !item.done"
+                >mdi-square-outline</v-icon
+              >
+              <v-icon
+                class="mr-3"
+                v-if="item.done === true"
+                @click="item.done = !item.done"
+                >mdi-square</v-icon
+              >
+              <div
+                class="txt"
+                @click="item.done = !item.done"
+                :class="item.done === true ? 'complete_todo' : ''"
+              >
+                {{ item.txt }}
+              </div>
+            </div>
+            <div class="button_container">
+              <v-icon @click="editTodo(item)" :disabled="item.done"
+                >mdi-pencil</v-icon
+              >
+              <v-icon @click="deleteTodo(item)">mdi-close</v-icon>
             </div>
           </div>
-          <div class="todo_detail" v-for="item in todoList" :key="item.id">
-            <div
-              class="
-                item_container
-                d-flex
-                justify-space-between
-                align-center
-                px-7
-                mx-2
-                mb-3
-                mt-4
-              "
-            >
-              <div class="txt_container d-flex">
-                <v-icon>mdi-square-outline</v-icon>
-                <v-icon>mdi-square</v-icon>
-                <div class="txt">{{ item.txt }}</div>
-              </div>
-              <div class="button_container">
-                <v-icon>mdi-pencil</v-icon>
-                <v-icon>mdi-close</v-icon>
-              </div>
+          <div
+            class="
+              item_edit_container
+              d-flex
+              justify-space-between
+              align-center
+              px-7
+              mx-2
+              mb-3
+              mt-4
+            "
+            v-if="todoField === item"
+          >
+            <input
+              type="text"
+              @keyup.enter="confirmEdit(item)"
+              class="edit_input px-4"
+              v-model="editInput"
+              :ref="`focusInput${item.id}`"
+              @keyup.esc="todoField = null"
+            />
+            <div class="button_container">
+              <v-icon @click="confirmEdit(item)">mdi-check</v-icon>
+              <v-icon @click="deleteTodo(item)">mdi-close</v-icon>
             </div>
-            <div
-              class="
-                item_edit_container
-                d-flex
-                justify-space-between
-                align-center
-                px-7
-                mx-2
-                mb-3
-                mt-4
-              "
-            >
-              <input type="text" class="edit_input px-4" />
-              <div class="button_container">
-                <v-icon>mdi-check</v-icon>
-                <v-icon>mdi-close</v-icon>
-              </div>
-            </div>
-            <v-divider inset class="mx-auto"></v-divider>
           </div>
+          <v-divider inset class="mx-auto"></v-divider>
+        </div>
+        <div class="total_todo px-7 mx-2 mt-4 d-flex justify-space-between">
+          <div>共 {{ todoStatus }} 個待完成項目</div>
+          <div class="complete" @click="clearComplete">清除已完成項目</div>
         </div>
       </div>
     </div>
@@ -98,9 +127,11 @@ export default {
   name: "todo",
   data() {
     return {
-      test: null,
-      testList: [],
+      newTodo: null,
+      todoField: null,
+      editInput: null,
       status: ["全部", "待完成", "已完成"],
+      choiceStatus: "全部",
       count: 0,
       todoList: [
         {
@@ -123,13 +154,81 @@ export default {
   },
   computed: {
     ...mapState("Home", ["nickname"]),
+    todoStatus() {
+      let number = 0;
+      this.filterTodoList.forEach((item) => {
+        if (!item.done) {
+          number++;
+        }
+      });
+      return number;
+    },
+    filterTodoList() {
+      let choiceStatusList = [];
+      switch (this.status[this.count]) {
+        case "全部":
+          return this.todoList;
+        case "待完成":
+          this.todoList.forEach((item) => {
+            if (!item.done) {
+              choiceStatusList.push(item);
+            }
+          });
+          return choiceStatusList;
+        case "已完成":
+          this.todoList.forEach((item) => {
+            if (item.done) {
+              choiceStatusList.push(item);
+            }
+          });
+          return choiceStatusList;
+        default:
+          return "";
+      }
+    },
   },
   methods: {
-    testfn() {
-      console.log("hahaha");
+    addTodo() {
+      if (this.newTodo) {
+        this.todoList.push({
+          id: Date.now(),
+          txt: this.newTodo,
+          done: false,
+        });
+      }
+      this.newTodo = null;
     },
-    choice_status(index) {
+    choice_status(item, index) {
+      this.choiceStatus = item;
       this.count = index;
+      this.todoField = null;
+    },
+    editTodo(data) {
+      this.todoField = data;
+      this.editInput = data.txt;
+      this.$nextTick(() => {
+        this.$refs[`focusInput${data.id}`][0].focus();
+      });
+    },
+    confirmEdit(data) {
+      data.txt = this.editInput;
+      this.todoField = null;
+    },
+    deleteTodo(data) {
+      this.todoList.forEach((item, index) => {
+        if (item.id === data.id) {
+          this.todoList.splice(index, 1);
+        }
+      });
+    },
+    clearComplete() {
+      let uncompleteList = [];
+      this.todoList.forEach((item) => {
+        if (!item.done) {
+          uncompleteList.push(item);
+        }
+      });
+      this.todoList = uncompleteList;
     },
   },
 };
@@ -140,11 +239,13 @@ export default {
   display: none;
 }
 #app {
+  height: 100vh;
   background: linear-gradient(
     172.7deg,
     #ffd370 5.12%,
     #ffd370 53.33%,
-    #ffffff 53.45%
+    #ffffff 53.45%,
+    #ffffff 94.32%
   );
   .top {
     height: 80px;
@@ -155,11 +256,7 @@ export default {
 
   .outside {
     width: 700px;
-    height: calc(100vh - 80px);
-    .inside {
-      width: 100%;
-      height: 500px;
-    }
+    margin-top: 30px;
   }
   .todo_content {
     background-color: white;
@@ -183,6 +280,18 @@ export default {
       .edit_input:focus {
         outline: none;
         border: 3px solid #ec8f3f;
+      }
+      .txt_container {
+        .complete_todo {
+          text-decoration: line-through;
+          color: #9f9a91;
+        }
+      }
+    }
+    .total_todo {
+      .complete {
+        color: #9f9a91;
+        cursor: pointer;
       }
     }
   }
